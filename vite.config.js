ff285a37fad04b57c459c33e831458ee84a3f697
Soschool <sean.oshea8@gmail.com>
2025-02-12 16:03:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/symptom-tracker/',  // This is important for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
