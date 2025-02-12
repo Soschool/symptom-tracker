228e32485834ce20de02a2e7a3e2d62d3d972048
Soschool <sean.oshea8@gmail.com>
2025-02-12 15:31:18 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SymptomTracker from './components/SymptomTracker'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SymptomTracker />
+    </div>
+  )
+}
+
+export default App
